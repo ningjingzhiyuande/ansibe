10ddc628069e74c9acf9a974678ac78481b96755
Yang Wen <kevin.wenyang@gmail.com>
2014-05-21 14:26:57 +0800

    update department code

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,9 @@ class User < ActiveRecord::Base
   has_enum :roles ,:enums => [[:user, 0, "用户"],[:chief, 100, "处长"],[:chairman, 200, "主任"]], \
       :column => :role, :default => :user
 
-  has_enum :departments ,:enums => [[:caigou, 0, "采购部Leaders"],[:zongguan, 1, "综合管理处"],[:jihua, 2, "计划处"],\
-           [:hetong, 3, "合同管理处"],[:jiyi, 4, "机械一处"],[:jier, 5, "机械二处"],[:yikong, 3, "仪控处"],[:dianqi, 6, "电气处"],\
-           [:jianzao, 7, "监造处"],[:zhibao, 8, "质量保证处"],[:xitong, 9, "系统设备及大宗材料处"]], 
+  has_enum :departments ,:enums => [[:caigou, 6100, "采购部主任"],[:zongguan, 6101, "综合管理处"],[:jihua, 6102, "计划处"],\
+           [:hetong, 6103, "合同管理处"],[:jiyi, 6104, "机械一处"],[:jier, 6105, "机械二处"],[:yikong, 6106, "仪控处"],[:dianqi, 6107, "电气处"],\
+           [:jianzao, 6108, "监造处"],[:zhibao, 6109, "质量保证处"],[:xitong, 6110, "系统设备及大宗材料处"]], 
       :column => :department, :default => :caigou
   #belongs_to :department
 
