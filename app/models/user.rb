8de9e3ce6fbe2825021186fce654e5d9b7c14513
Yang Wen <kevin.wenyang@gmail.com>
2014-05-22 14:22:51 +0800

    comment out department has_enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,11 +15,11 @@ class User < ActiveRecord::Base
   has_enum :roles ,:enums => [[:user, 0, "员工"],[:chief, 100, "处长"],[:chairman, 200, "主任"]], \
       :column => :role, :default => :user
 
-  #has_enum :departments ,:enums => [[:caigou, 6100, "采购部主任"],[:zongguan, 6101, "综合管理处"],[:jihua, 6102, "计划处"],\
-   #        [:hetong, 6103, "合同管理处"],[:jiyi, 6104, "机械一处"],[:jier, 6105, "机械二处"],[:yikong, 6106, "仪控处"],[:dianqi, 6107, "电气处"],\
-   #        [:jianzao, 6108, "监造处"],[:zhibao, 6109, "质量保证处"],[:xitong, 6110, "系统设备及大宗材料处"]], 
-  #    :column => :department
-  #belongs_to :department
+  has_enum :departments ,:enums => [[:caigou, 6100, "采购部主任"],[:zongguan, 6101, "综合管理处"],[:jihua, 6102, "计划处"],\
+          [:hetong, 6103, "合同管理处"],[:jiyi, 6104, "机械一处"],[:jier, 6105, "机械二处"],[:yikong, 6106, "仪控处"],[:dianqi, 6107, "电气处"],\
+          [:jianzao, 6108, "监造处"],[:zhibao, 6109, "质量保证处"],[:xitong, 6110, "系统设备及大宗材料处"]], 
+     :column => :department
+  belongs_to :department
 
   def admin?
   	#true
