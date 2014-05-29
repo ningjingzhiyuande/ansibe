74c3ba1627f72b90ed6eaa97cb800f561d780dfd
Yang Wen <kevin.wenyang@gmail.com>
2014-05-29 10:08:26 +0800

    update cnpe email config

--- app/mailers/entretain_mail.rb
+++ app/mailers/entretain_mail.rb
@@ -1,22 +1,22 @@
 class EntretainMail < ActionMailer::Base
-  default from: "oreatial@gmail.com"
+  default from: "cgboa@cnpe.cc"
  # @url="http://localhost:5000"
 
   def send_apply_mail(entretain)
-  	@url="http://localhost:5000"
+  	@url="http://10.30.62.247:3000"
   	  @entretain = entretain
       mail to: entretain.report_user.try(:email), subject: entretain.user.try(:name)+"用餐申请单,请您审阅"
   end
 
   def send_finished_mail(entretain)
   	@entretain = entretain
-  	@url="http://localhost:5000"
+  	@url="http://10.30.62.247:3000"
     mail to: entretain.user.try(:email), subject: entretain.user.try(:name)+"宴请单已被批阅"
   end
 
   def send_superior_mail(entretain)
 	@entretain = entretain
-	@url="http://localhost:5000"
+	@url="http://10.30.62.247:3000"
 	
     mail to: entretain.last_report_user.try(:email), subject: entretain.user.try(:name)+"宴请单已被#{entretain.report_user.try(:name)}批阅,请您最终审批"
   end
