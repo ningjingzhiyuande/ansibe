bcbf2c12b60aeb5bb5b307ff16f6c5bb983247c7
Yang Wen <kevin.wenyang@gmail.com>
2014-06-09 09:05:31 +0800

    update sending email address

--- app/mailers/entretain_mail.rb
+++ app/mailers/entretain_mail.rb
@@ -3,20 +3,20 @@ class EntretainMail < ActionMailer::Base
  # @url="http://localhost:5000"
 
   def send_apply_mail(entretain)
-  	@url="http://10.30.62.247:3000"
+  	@url="http://cgboffice.cnpe.cc"
   	  @entretain = entretain
       mail to: entretain.report_user.try(:email), subject: entretain.user.try(:name)+"用餐申请单,请您审阅"
   end
 
   def send_finished_mail(entretain)
   	@entretain = entretain
-  	@url="http://10.30.62.247:3000"
+  	@url="http://cgboffice.cnpe.cc"
     mail to: entretain.user.try(:email), subject: entretain.user.try(:name)+"宴请单已被批阅"
   end
 
   def send_superior_mail(entretain)
 	@entretain = entretain
-	@url="http://10.30.62.247:3000"
+	@url="http://cgboffice.cnpe.cc"
 	
     mail to: entretain.last_report_user.try(:email), subject: entretain.user.try(:name)+"宴请单已被#{entretain.report_user.try(:name)}批阅,请您最终审批"
   end
